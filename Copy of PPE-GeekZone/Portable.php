a706e015279ce93b6a188b6e7137308f62cafe6d
Alexandre.Peillex <alex.peillex@Gmail.com>
2016-02-10 03:26:37 +0100

    Update 1.02 (ajout des pages Cuisine, USB, Mode + modification de la page Portable)

--- Copy of PPE-GeekZone/Portable.php
+++ Copy of PPE-GeekZone/Portable.php
@@ -1,24 +1,24 @@
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Portable</title>
+<title>Portables</title>
 <link rel="stylesheet" type="text/css" href="css/Portable.css" />
 <link rel="stylesheet" type="text/css" href="css/css-geek.css" />
 </head>
 <body>
-	<?php $nav_en_cours = 'Portable'; ?> 	<!-- Pour savoir la page en cour -->
+	<?php $nav_en_cours = 'Portables'; ?> 	<!-- Pour savoir la page en cour -->
 	<?php include 'include/barre-menu.inc.php' ?> 	<!-- Background + titre + rectangle avec le menu -->
 	<div class = "photoOpacity"></div>
 	<div class="photoCadre">
-		<div class=titrePhoto> Nos <span class="espace"> Gadgets <span class="espace"> ! </span></span></div>
+		<div class=titrePhoto> Nos <span class="espace"> Portables <span class="espace"> ! </span></span></div>
 		<div class = "portable1">
-			<a href="Portable1.php" title="Gadget1"><img class = "imgPortable1 articleS" src = "img/portable/coque-iphone-4-decapsuleur-2.jpg" /></a>
+			<a href="Portable1.php" title="Portable1"><img class = "imgPortable1 articleS" src = "img/portable/coque-iphone-4-decapsuleur-2.jpg" /></a>
 		</div>
 		<div class = "portable2">
-			<a href="Portable2.php" title="Gadget2"><img class = "imgPortable2 articleS" src = "img/portable/manette-jeu-pour-ecran-tactile-istick-1.jpg" /></a>
+			<a href="Portable2.php" title="Portable2"><img class = "imgPortable2 articleS" src = "img/portable/manette-jeu-pour-ecran-tactile-istick-1.jpg" /></a>
 		</div>
 		<div class = "portable3">
-			<a href="Portable3.php" title="Gadget3"><img class = "imgPortable3 articleS" src = "img/portable/ventilo-pour-iphone.jpg" /></a>
+			<a href="Portable3.php" title="Portable3"><img class = "imgPortable3 articleS" src = "img/portable/ventilo-pour-iphone.jpg" /></a>
 		</div>
 	</div>
 	<?php include 'include/pied-page.inc.php' ?> 	<!-- Bas de page  -->
--- Copy of PPE-GeekZone/include/barre-menu.inc.php
+++ Copy of PPE-GeekZone/include/barre-menu.inc.php
@@ -11,20 +11,20 @@
 	<h1 class="menu"> M E N U </h1> 	<!-- Menu avec les images + liens + 
 											code pour montrer la page en cour + survol de la souris -->
 												
-	<a href="../" title="Vêtements"><img class="boutique menuS" src="img/boutique.png" alt="MenuBoutique" 
-		<?php if ($nav_en_cours == 'Boutique'){ echo 'id="en-cours"';} ?> /></a>
+	<a href="Mode.php" title="Mode"><img class="boutique menuS" src="img/boutique.png" alt="MenuBoutique" 
+		<?php if ($nav_en_cours == 'Mode'){ echo 'id="en-cours"';} ?> /></a>
 	
 	<a href="Gadget.php" title="Gadgets"><img class="gadget menuS" src="img/gadget.png" alt="MenuGadget" 
 		<?php if ($nav_en_cours == 'Gadget'){ echo 'id="en-cours"';} ?> /></a>
 
-	<a href="../" title="Divers"><img class="manger menuS" src="img/manger.png" alt="MenuManger" 
-		<?php if ($nav_en_cours == 'Divers'){ echo 'id="en-cours"';} ?> /></a>
+	<a href="Cuisine.php" title="Cuisine"><img class="manger menuS" src="img/manger.png" alt="MenuManger" 
+		<?php if ($nav_en_cours == 'Cuisine'){ echo 'id="en-cours"';} ?> /></a>
 	
-	<a href="Portable.php" title="Téléphones"><img class="phone menuS" src="img/phone.png" alt="MenuPhone" 
-		<?php if ($nav_en_cours == 'Téléphone'){ echo 'id="en-cours"';} ?> /></a>
+	<a href="Portable.php" title="Portables"><img class="phone menuS" src="img/phone.png" alt="MenuPhone" 
+		<?php if ($nav_en_cours == 'Portables'){ echo 'id="en-cours"';} ?> /></a>
 
-	<a href="../" title="Usb"><img class="usb menuS" src="img/usb.png" alt="MenuUsb" 
-		<?php if ($nav_en_cours == 'Usb'){ echo 'id="en-cours"';} ?> /></a> 
+	<a href="USB.php" title="USB"><img class="usb menuS" src="img/usb.png" alt="MenuUsb" 
+		<?php if ($nav_en_cours == 'USB'){ echo 'id="en-cours"';} ?> /></a> 
 
 	<a href="Index.php" title="Accueil"><img class="accueil menuS" src="img/accueilgeek.png" alt="MenuAccueil" 
 		<?php if ($nav_en_cours == 'Accueil'){ echo 'id="en-cours"';} ?> /></a>
