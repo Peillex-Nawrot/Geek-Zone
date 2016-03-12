f518d835e9e323ceec8084f6102c1fb3bdc2db5e
guillaume <guillaume.nawrot.pro@gmail.com>
2016-03-12 02:19:39 +0100

    Version 1.4

---
+++ PPE-GeekZone/Portable.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Portables</title>
+<link rel="stylesheet" type="text/css" href="css/Portable.css" />
+<link rel="stylesheet" type="text/css" href="css/css-geek.css" />
+</head>
+<body>
+	<?php $nav_en_cours = 'Portables'; ?> 	<!-- Pour savoir la page en cour -->
+	<?php include 'include/barre-menu.inc.php' ?> 	<!-- Background + titre + rectangle avec le menu -->
+	<div class = "photoOpacity"></div>
+	<div class="photoCadre">
+		<div class=titrePhoto> Nos <span class="espace"> Portables <span class="espace"> ! </span></span></div>
+		<div class = "portable1">
+			<a href="descPortable1.php" title="Portable1"><img class = "imgPortable1 articleS" src = "img/portable/coque-iphone-4-decapsuleur.jpg" /></a>
+		</div>
+		<div class = "portable2">
+			<a href="descPortable2.php" title="Portable2"><img class = "imgPortable2 articleS" src = "img/portable/manette-jeu-pour-ecran-tactile-istick.jpg" /></a>
+		</div>
+		<div class = "portable3">
+			<a href="descPortable3.php" title="Portable3"><img class = "imgPortable3 articleS" src = "img/portable/ventilo-pour-iphone.jpg" /></a>
+		</div>
+	</div>
+	<?php include 'include/pied-page.inc.php' ?> 	<!-- Bas de page  -->
+</body>
+</html>
